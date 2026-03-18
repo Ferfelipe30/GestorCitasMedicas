7afd185f91805ba8d8e6b3412c460ad2d3c74c3c
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2026-03-18 15:34:14 -0500

    Conexion de base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
 
 app.use(express.json());
@@ -6,6 +7,15 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'API Express funcionando' });
 });
+
+app.get('/api/health/db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW() AS now');
+    res.json({ ok: true, db: 'up', now: result.rows[0].now });    
+  }catch (error) {
+    res.status(500).json({ ok: false, db: 'down', error: error.message });
+  }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
