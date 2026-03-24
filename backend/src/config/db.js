3c8465b8120873580f5355fb67a1b205c354698b
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2026-03-24 12:04:53 -0500

    Actualizacion de de conexion con la base de datos

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {rejectUnauthorized: false}
+});
+
+pool.connect()
+    .then(() => console.log('Conectado a la base de datos PostgreSQL de Google cloud'))
+    .catch(err => console.error('Error de conexion: ', err));
+
+module.exports = pool;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const pool = require('./db');
-const app = express();
+const pool = require('./config/db');
+require('dotenv').config();
 
+const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'API Express funcionando' });
-});
-
-app.get('/api/health/db', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW() AS now');
-    res.json({ ok: true, db: 'up', now: result.rows[0].now });    
-  }catch (error) {
-    res.status(500).json({ ok: false, db: 'down', error: error.message });
-  }
 });
 
 app.get('/api/usuarios', async (_req, res) => {
